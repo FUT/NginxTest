d91b4a51261f471a7a14625a38bb0ed90b63cd04
Jura Koleda <fut.wrk@gmail.com>
2011-11-14 10:44:54 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NginxTest::Application.config.secret_token = '587886d0501c6cbbba9712bbb4e3bf9738f563659c703cff5dde4fc1ae719c5805302d4ad4ea085bc016c625a3d9a57a1b965c7cbd0a7fab1f69336cb3466f54'
